6210378f007ffd265c7365d056079896923857c0
Henry Xu <henry.xu@ringcentral.com>
2018-06-01 11:28:58 +0800

    fix login

--- glipbot/handlers/glip.py
+++ glipbot/handlers/glip.py
@@ -1,3 +1,4 @@
+import json
 from . import BaseHandler
 from ..services import glip
 from .. import config
@@ -13,7 +14,8 @@ class BaseGlipHandler(BaseHandler):
 class GlipAuthHandler(BaseHandler):
     async def get(self, *args, **kwargs):
         code = self.get_argument('code')
-        glip.service.login(username=config.RC_BOT_ACCOUNT,
+        glip.service.login(username=config.RC_BOT_NUMBER,
+                           extension=config.RC_BOT_EXTENSION,
                            code=code,
                            redirect_uri=config.RC_AUTH_REDIRECT_URI,
                            )
@@ -29,8 +31,9 @@ class GlipEventsHandler(BaseHandler):
         if verification_token != config.RC_WEBHOOK_TOKEN:
             return self.send_error(401)
         validation_token = self.request.headers.get(self.VALIDATION_TOKEN_HEADER)
-        # TODO: dispatch message here
-        logger.info("body is: %s", self.request.body)
         if validation_token is not None:
             self.set_header(self.VALIDATION_TOKEN_HEADER, validation_token)
         self.finish()
+        logger.info("body is: %s", self.request.body)
+        if self.request.body:
+            glip.service.dispatch(json.loads(self.request.body))
--- glipbot/services/glip.py
+++ glipbot/services/glip.py
@@ -1,3 +1,5 @@
+import pickle
+import os
 from ringcentral.sdk import SDK
 
 from .. import config
@@ -8,15 +10,25 @@ logger = logging.getLogger(__name__)
 
 
 class GlipService(object):
-
     def __init__(self, rc_platform):
         self._rc_helper = RcPlatformHelper(rc_platform)
 
-    def login(self, username, code, redirect_uri):
-        self._rc_helper.platform.login(username=username, code=code, redirect_uri=redirect_uri)
+    def login(self, username, extension, code, redirect_uri):
+        self._rc_helper.platform.login(username=username, extension=extension, code=code, redirect_uri=redirect_uri)
+        with open(config.RC_AUTH_TOKEN_CACHE, mode='wb') as f:
+            auth = self._rc_helper.platform.auth()
+            pickle.dump(auth, f)
 
     def subscribe_webhook(self, address, event_filters=None, expires_in=50000000):
         self._rc_helper.subscribe_webhook(address=address, event_filters=event_filters, expires_in=expires_in)
+
+    def dispatch(self, post):
+        creator_id = post["body"]["creatorId"]
+        if creator_id == self._rc_helper.me["id"]:
+            return None
+        self._rc_helper.post_to_group(post["body"]["groupId"], post["body"]["text"])
+
+
 
 
 _sdk = SDK(key=config.RC_KEY,
@@ -24,4 +36,7 @@ _sdk = SDK(key=config.RC_KEY,
            server=config.RC_SERVER,
            )
 _platform = _sdk.platform()
+if os.path.exists(config.RC_AUTH_TOKEN_CACHE):
+    with open(config.RC_AUTH_TOKEN_CACHE, mode='rb') as f:
+        _platform._auth = pickle.load(f)
 service = GlipService(_platform)
